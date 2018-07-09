b88811c53791be87786916f538da32c446c25d1a
Jason Viger <jason.viger@gmail.com>
2018-07-09 12:52:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var recipes = {};
 
 var updateObjectWithKeyAndValue = function(object,key,value){
-  
-  object[key] = value;
-  return object;
+  var newObject = object;
+  newObject[key] = value;
+  return newObject;
 }
 
 var destructivelyUpdateObjectWithKeyAndValue = function(object,key,valuev){
