e0f83d35f3c6696499c992c7f16c8bdc5962fe8f
Jason Viger <jason.viger@gmail.com>
2018-07-09 12:55:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,9 @@ var destructivelyUpdateObjectWithKeyAndValue = function(object,key,value){
   object[key] = value;
   return object;
 }
-
+var deleteFromObjectByKey = function(object,key){
+  
+  var newObject = object;
+  delete newObject.key;
+  return newObject;
+}
